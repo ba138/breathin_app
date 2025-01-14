30a44c5503f5d5cdf415b2df02918d6767c0d4e1
Basit ali <basitalyshah51214@gmail.com>
2025-01-14 02:47:20 -0800

    Initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'Utills/app_theme.dart';
 import 'Views/LoginView/login_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
